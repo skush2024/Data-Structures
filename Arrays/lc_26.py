e03276ea31ff72f1d42e38cfc8e862978fb33152
skush2024 <kushashah50@gmail.com>
2026-03-15 16:18:47 -0700

    LC 26 Remove Duplicate From an Array

---
+++ Arrays/lc_26.py
@@ -0,0 +1,28 @@
+"""
+Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same.
+
+Consider the number of unique elements in nums to be k​​​​​​​​​​​​​​. After removing duplicates, return the number of unique elements k.
+
+The first k elements of nums should contain the unique numbers in sorted order. The remaining elements beyond index k - 1 can be ignored.
+"""
+
+class Solution:
+
+    def __init__(self):
+        pass
+
+    def solve(self, nums):
+        i = 0
+
+        for j in range(1, len(nums)):
+            if nums[j] != nums[i]:
+                i += 1
+                nums[i] = nums[j]
+
+        return i + 1
+
+
+if __name__ == "__main__":
+    nums = [1,1,2,2,2,3]
+    solver = Solution()
+    solver.solve(nums)
