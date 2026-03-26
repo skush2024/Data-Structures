432dbb23aa8627fd93a4d0420db94e9be917be84
skush2024 <kushashah50@gmail.com>
2026-03-26 14:55:53 -0400

    LC 90 Subsets 2

--- Recursion/lc_90.py
+++ Recursion/lc_90.py
@@ -9,33 +9,33 @@ class Solution:
     def __init__(self):
         pass
 
-
-    def subsetsWithDupl(self, nums, idx, output,ans):
-        if len(nums) == idx :
-            output.append(ans.copy())
-            return
     
-        
-        ans.append(nums[idx])
-        self.subsetsWithDupl(nums, idx + 1, output, ans)
-        
-        ans.pop()
-        i = idx + 1
-        
-        while(i < len(nums) and nums[i] == nums[i - 1]) : 
-            i+= 1
-        
-        self.subsetsWithDupl(nums, i, output, ans)
-        
+
     def subsets(self, nums):
         nums.sort()
-        output = []
-        self.subsetsWithDupl(nums, 0, output, [])
-        print(output)
+        result = []
         
+
+        def explore(subsets, idx):
+            result.append(subsets.copy())
+
+            for i in range(idx, len(nums)):
+                
+                if i > idx and nums[i] == nums[i - 1]:
+                    continue
+                
+                subsets.append(nums[i])
+                explore(subsets, i + 1)
+                subsets.pop()
+            
+        explore([],0)
+        return result 
 
 if __name__ == "__main__":
     solver = Solution()
 
     nums = [1,2,2]
-    solver.subsets(nums)
+    # solver.subsets(nums)
+    
+    result = solver.subsets(nums)
+    print(result)
--- Recursion/recursion.py
+++ Recursion/recursion.py
@@ -9,8 +9,8 @@ class Recursion:
             print(n)
 
         else :
-            print(n)
             self.log_N(n - 1)
+            print(n)
 
     
     def factorial(self, n):
@@ -35,9 +35,65 @@ class Recursion:
         self.findSums(arr, 0,0,sums)
         print(sums)
 
+    def fibonaci(self, n):
+        if n <= 1: 
+            return n
+        
+        else :
+            return self.fibonaci(n - 1) + self.fibonaci(n - 2) 
+        
+    def check_sorted(self, arr, idx):
+        if idx == 1 :
+            if arr[idx] >= arr[idx - 1]:
+                return True
+
+            return False
+        
+        else:
+            return arr[idx] >= arr[idx - 1] and self.check_sorted(arr, idx - 1)
+
+
+    def binary_search(self, arr, target, low, high):
+        if low <= high :
+            mid = int((low + high) / 2)
+            if arr[mid] == target:
+                return mid
+            
+            else :
+                if arr[mid] > target:
+                    return self.binary_search(arr, target, low, mid - 1)
+                else :
+                    return self.binary_search(arr, target, mid + 1, high)
+
+
+        return -1
+
+
+    def make_words(self, choices):
+        result = []
+        
+        def explore(path):
+            if len(path) == len(choices):
+                result.append(path.copy())
+                return 
+            
+            for choice in choices:
+                if choice in path :
+                    continue
+                path.append(choice)
+                explore(path)
+                path.pop()
+
+        explore([])
+        return result
 
 
 if __name__ == "__main__" :
     executor = Recursion()
+    arr = [1,2,3,3,4]
 
-    print(executor.findSubsetSums([1,2,3]))
+    # print(executor.findSubsetSums([1,2,3]))
+    # print(executor.check_sorted(arr, len(arr) - 1))
+    # print(executor.binary_search(arr, 1,0, len(arr) - 1))
+    print(executor.make_words(['k','u','s','h']))
+    print(len(executor.make_words(['k','u','s','h'])))
