61bba08910013df980abecadb4d7e755b0f3367c
skush2024 <kushashah50@gmail.com>
2026-04-04 02:53:10 -0700

    LC 33 Search in Rotated Array

---
+++ Binary-Search/lc_33.py
@@ -0,0 +1,49 @@
+"""
+There is an integer array nums sorted in ascending order (with distinct values).
+
+Prior to being passed to your function, nums is possibly left rotated at an unknown index k (1 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,5,6,7] might be left rotated by 3 indices and become [4,5,6,7,0,1,2].
+
+Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
+
+You must write an algorithm with O(log n) runtime complexity.
+"""
+
+class Solution:
+
+    def __init__(self):
+        pass
+    
+
+    def search(self, nums, target):
+        low = 0
+        high = len(nums) - 1
+
+        while low <= high :
+            mid = int((low + high) / 2)
+
+            if nums[mid] == target :
+                return mid
+
+            # If Left Part is Sorted
+            if nums[mid] >= nums[low]:
+                if nums[low] <= target and target < nums[mid] :
+                    high = mid - 1
+                
+                else :
+                    low = mid + 1
+            
+            # If Right Part is Sorted
+            else :
+                if nums[mid] < target and target <= nums[high]:
+                    low = mid + 1
+                else :
+                    high = mid - 1
+
+        return -1
+        
+
+if __name__ == "__main__":
+    solver = Solution()
+
+    nums = [4,5,6,7,8,9,2]
+    print(solver.search(nums, 7))
